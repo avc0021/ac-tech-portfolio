e16bd9f0dc4a85ea2cc8d38cb8bb93ccb24cd17b
Adam Castro <ajcastr1@uiwtx.edu>
2024-01-02 15:27:27 -0600

    init

---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      {/* List your projects here */}
+    </div>
+  );
+}
+
+export default Projects;
