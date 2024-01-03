93fed8d43ae24dc0de8915ab73b7e9f578b98c2d
Adam Castro <ajcastr1@uiwtx.edu>
2024-01-03 16:22:03 -0600

    updated contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,10 @@ function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>Feel free to reach out via email at...</p>
+      <p>Phone: (210) 383-0681</p>
+      <p>Email: <a href="mailto:avc0021@gmail.com">avc0021@gmail.com</a></p>
+      <p><a href="https://github.com/ajcastr1">Visit My GitHub Work Profile</a></p>
+      <p><a href="https://github.com/avc0021">Visit My GitHub Personal Profile</a></p>
     </div>
   );
 }
