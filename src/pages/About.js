1eb5391e6d895980dcddc9999db873a1e0e6c32c
Adam Castro <ajcastr1@uiwtx.edu>
2024-01-08 21:27:04 -0600

    added container for content

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,25 +2,27 @@ import React from "react";
 
 function About() {
   return (
-    <div className="content">
-      <h1>About Me</h1>
-      <p>
-        Hello there! My name is Adam and I've found my calling in the vibrant
-        world of web and mobile technology at the University of the Incarnate
-        Word. Here, I'm not just coding or designing; I'm reimagining how we
-        interact with digital spaces. I love diving into projects headfirst,
-        whether it's bringing a new app to life or finding a fresh twist on a
-        website. It's all about making things that people love to use. My tech
-        toolbox includes Javascript, Cascade CMS, AWS Lambda, Git, Microsoft
-        Azure, Algolia, MaterialUI, Bootstrap, NodeJS, GraphQL, Canva,
-        Photoshop, and React. For me, it's not just about the tools. Its about
-        creating user-friendly tech marvels. I believe the best tech solutions
-        are those that feel natural and intuitive – almost like they've always
-        been a part of our lives. Collaboration is key in my book and I've
-        always been a fan of brainstorming sessions where ideas bounce off the
-        walls, and everyone has a voice. It's amazing what we can come up with
-        when we put our minds together!
-      </p>
+    <div className="container">
+      <div className="content">
+        <h1>About</h1>
+        <p>
+          Hello there! My name is Adam and I've found my calling in the vibrant
+          world of web and mobile technology at the University of the Incarnate
+          Word. Here, I'm not just coding or designing; I'm reimagining how we
+          interact with digital spaces. I love diving into projects headfirst,
+          whether it's bringing a new app to life or finding a fresh twist on a
+          website. It's all about making things that people love to use. My tech
+          toolbox includes Javascript, Cascade CMS, AWS Lambda, Git, Microsoft
+          Azure, Algolia, MaterialUI, Bootstrap, NodeJS, GraphQL, Canva,
+          Photoshop, and React. For me, it's not just about the tools. Its about
+          creating user-friendly tech marvels. I believe the best tech solutions
+          are those that feel natural and intuitive – almost like they've always
+          been a part of our lives. Collaboration is key in my book and I've
+          always been a fan of brainstorming sessions where ideas bounce off the
+          walls, and everyone has a voice. It's amazing what we can come up with
+          when we put our minds together!
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,38 +1,58 @@
 import React from "react";
-import { Box, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import { Box, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 
 function Contact() {
   return (
+    <div className="container">
     <div className="content">
- <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <ListItem button component="a" href="https://github.com/ajcastr1" target="_blank">
-            <ListItemIcon>
-              <GitHubIcon />
-            </ListItemIcon>
-            <ListItemText primary="GitHub- Work" />
-          </ListItem>
-          <ListItem button component="a" href="https://github.com/avc0021" target="_blank">
-            <ListItemIcon>
-              <GitHubIcon />
-            </ListItemIcon>
-            <ListItemText primary="GitHub- Personal" />
-          </ListItem>
-          <ListItem button component="a" href="mailto:avc0021@gmail.com">
-            <ListItemIcon>
-              <EmailIcon />
-            </ListItemIcon>
-            <ListItemText primary="Email" />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <PhoneIcon />
-            </ListItemIcon>
-            <ListItemText primary="(210) 383-0681" />
-          </ListItem>
-        </Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <ListItem
+          button
+          component="a"
+          href="https://github.com/ajcastr1"
+          target="_blank"
+        >
+          <ListItemIcon>
+            <GitHubIcon />
+          </ListItemIcon>
+          <ListItemText primary="GitHub- Work" />
+        </ListItem>
+        <ListItem
+          button
+          component="a"
+          href="https://github.com/avc0021"
+          target="_blank"
+        >
+          <ListItemIcon>
+            <GitHubIcon />
+          </ListItemIcon>
+          <ListItemText primary="GitHub- Personal" />
+        </ListItem>
+        <ListItem button component="a" href="mailto:avc0021@gmail.com">
+          <ListItemIcon>
+            <EmailIcon />
+          </ListItemIcon>
+          <ListItemText primary="Email" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <PhoneIcon />
+          </ListItemIcon>
+          <ListItemText primary="(210) 383-0681" />
+        </ListItem>
+      </Box>
+    </div>
     </div>
   );
 }
